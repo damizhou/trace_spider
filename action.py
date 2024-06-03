52f5f865be2515b336bd31f4b06be8a1b47649e5
pcz <598069751@qq.com>
2024-06-03 16:48:32 +0800

    优化定时器逻辑，爬虫提前结束定时器也会提前结束

--- action.py
+++ action.py
@@ -11,6 +11,8 @@ from datetime import datetime
 from utils.task import task_instance
 
 duration = int(config["spider"]["duration"])
+process = CrawlerProcess(get_project_settings())
+crawlers_timer = None
 
 
 # 清除浏览器进程
@@ -34,24 +36,38 @@ def traffic():
     capture(allowed_domain, formatted_time)
 
 
+# 停止爬虫
+def stop_crawlers():
+    logger.info("定时器触发，停止所有爬虫")
+    global crawlers_timer
+    for crawler in process.crawlers:
+        crawler.stop()
+    crawlers_timer = None
+
+
+# 启动定时器
+def stop_crawlers_after_delay():
+    global crawlers_timer
+    crawlers_timer = threading.Timer(duration, stop_crawlers)
+    crawlers_timer.start()
+
+
+# 取消定时器
+def cancel_timer():
+    global crawlers_timer
+    if crawlers_timer is not None:
+        logger.info(f"爬虫提前结束，关闭定时器")
+        crawlers_timer.cancel()
+
+
 # 启动爬虫
 def start_spider():
-    def stop_crawlers_after_delay(process, delay):
-        def stop_crawlers():
-            logger.info("定时器触发，停止所有爬虫")
-            for crawler in process.crawlers:
-                crawler.stop()
-
-        timer = threading.Timer(delay, stop_crawlers)
-        timer.start()
-
-    process = CrawlerProcess(get_project_settings())
-
     # 添加你要运行的爬虫
     process.crawl(TraceSpider)
 
     logger.info(f"开始爬取数据")
-    stop_crawlers_after_delay(process, duration)
+    # 开启定时器
+    stop_crawlers_after_delay()
     # 启动爬虫
     process.start()
 
@@ -66,19 +82,19 @@ def start_task():
 
     start_spider()
     logger.info(f"爬取数据结束, 等待10秒.让浏览器加载完所有已请求的页面")
-    time.sleep(10)
+    time.sleep(1)
 
     logger.info(f"清理浏览器进程")
     kill_chrome_processes()
     logger.info(f"等待TCP结束挥手完成")
-    time.sleep(60)
+    time.sleep(1)
 
     # 关流量收集
     logger.info(f"关流量收集")
     stop_capture()
 
     logger.info(f"{task_instance.current_start_url}流量收集结束，共爬取{task_instance.requesturlNum}个页面")
-
+    cancel_timer()
 
 if __name__ == "__main__":
     start_task()
