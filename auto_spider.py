0ce52585a99c91e0bdeb283f54d5ad277af6bad3
pcz <598069751@qq.com>
2024-12-13 17:52:03 +0800

    更新代码

--- auto_spider.py
+++ auto_spider.py
@@ -64,7 +64,7 @@ def handle_server(server):
             # f"echo '{password}' | sudo -S apt install -y docker.io",
             f"docker stop $(docker ps -q)",
             f"docker rm -f $(docker ps -a -q)",
-            f"echo '{password}' | sudo rm -rf trace_spider* spiderCode",
+            f"echo '{password}' | sudo -S rm -rf trace_spider* spiderCode",
             f"echo '{password}' | sudo -S ethtool -K docker0 tso off gso off gro off",
             f'git clone --branch novpn https://github.com/damizhou/trace_spider.git spiderCode',
             f'git clone https://github.com/damizhou/clash-for-linux.git spiderCode/clash-for-linux',
