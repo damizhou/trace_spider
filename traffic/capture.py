3334c4880857f926ded2099bd7b72533436fb2a7
pcz <598069751@qq.com>
2025-09-12 10:01:33 +0800

    提交测试代码

--- traffic/capture.py
+++ traffic/capture.py
@@ -20,9 +20,7 @@ def capture(TASK_NAME, formatted_time, parsers):
     traffic_dir = os.path.join(dataDir, TASK_NAME)
     os.makedirs(traffic_dir, exist_ok=True)
     os.chown(traffic_dir, int(os.getenv('HOST_UID')), int(os.getenv('HOST_GID')))
-    filename = ''
-    for parser in parsers:
-        filename += f"{parser}_"
+    filename = f"{parsers}_"
 
     traffic_name = os.path.join(traffic_dir, f"{filename}{formatted_time}_{TASK_NAME}.pcap")
 
