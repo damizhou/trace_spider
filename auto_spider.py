9b7edebdc06e62714a96f26a8292f8c852b8ffe7
pcz <598069751@qq.com>
2025-09-14 16:10:56 +0800

    提交代码

--- auto_spider.py
+++ auto_spider.py
@@ -61,7 +61,7 @@ def handle_server(server):
             # f"echo '{password}' | sudo -S apt install -y docker.io",
             f"docker stop $(docker ps -q -f \"name=^trace_spider\") | docker rm -f $(docker ps -aq -f \"name=^trace_spider\")",
             f"echo '{password}' | sudo -S rm -rf trace_spider* spiderCode",
-            # f"echo '{password}' | sudo -S ethtool -K docker0 tso off gso off gro off",
+            f"echo '{password}' | sudo -S ethtool -K docker0 tso off gso off gro off",
             f'git clone --branch ssl_key_csv_github https://github.com/damizhou/trace_spider.git spiderCode',
             # f'git clone https://github.com/damizhou/clash-for-linux.git spiderCode/clash-for-linux',
         ]
