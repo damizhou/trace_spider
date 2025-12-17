33aa910f492e1bc61720cbd5ee9a9615de8cebd7
pcz <598069751@qq.com>
2025-12-17 16:50:45 +0800

    提交代码

--- traffic/capture.py
+++ traffic/capture.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from utils.task import task_instance
 
 should_stop_capture = False
+process = None
 
 
 def capture(TASK_NAME, formatted_time, parsers):
@@ -43,6 +44,9 @@ def capture(TASK_NAME, formatted_time, parsers):
 
 def stop_capture():
     global process
+    if process is None:
+        logger.warning("capture 未启动，无需停止")
+        return None
     # 取输出文件路径
     pid = process.pid
     p = psutil.Process(pid)
